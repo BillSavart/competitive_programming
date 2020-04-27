c12e71b8df93099392b65ad07a21721fd8f70c9c
BillSavart <energyy0512@gmail.com>
2020-04-27 15:25:36 +0800

    prepared

--- poker.cpp
+++ poker.cpp
@@ -5,16 +5,21 @@ using namespace std;
 
 void check_card(string b1, string b2, string b3, string b4, string b5, string w1, string w2, string w3, string w4, string w5){
     int black_num[5], white_num[5];
-    string black_suit[5], white_suit[5];
+    string black_suit = "", white_suit = "";
     
+    //number of the cards
     black_num[0] = b1[0] - '0'; black_num[1] = b2[0] - '0'; black_num[2] = b3[0] - '0'; black_num[3] = b4[0] - '0'; black_num[4] = b5[0] - '0';
-    black_suit[0] = b1[1]; black_suit[1] = b2[1]; black_suit[2] = b3[1]; black_suit[3] = b4[1]; black_suit[4] = b5[1];
-
     white_num[0] = w1[0] - '0'; white_num[1] = w2[0] - '0'; white_num[2] = w3[0] - '0'; white_num[3] = w4[0] - '0'; white_num[4] = w5[0] - '0';
-    white_suit[0] = w1[1]; white_suit[1] = w2[1]; white_suit[2] = w3[1]; white_suit[3] = w4[1]; white_suit[4] = w5[1];
 
     sort(black_num, black_num + 5);
     sort(white_num, white_num + 5);
+
+    //suit of the cards
+    black_suit += b1[1]; black_suit += b2[1]; black_suit += b3[1]; black_suit += b4[1]; black_suit += b5[1];
+    white_suit += w1[1]; white_suit += w2[1]; white_suit += w3[1]; white_suit += w4[1]; white_suit += w5[1];
+
+    sort(black_suit.begin(), black_suit.end());
+    sort(white_suit.begin(), white_suit.end());
 
 }
 void check_winner(){
